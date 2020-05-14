7ea4351e316f590848cebb338bf1e2309b30bf79
levin <3259963@qq.com>
2020-05-14 18:02:58 +0800

    update

--- src/react-auth0-spa.tsx
+++ src/react-auth0-spa.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import createAuth0Client, { Auth0ClientOptions } from '@auth0/auth0-spa-js';
 
+type callbackFunction = {
+  (): void;
+};
 export type Auth0ClientProps = {
-  onRedirectCallback: () => void;
+  onRedirectCallback: callbackFunction;
 } & Auth0ClientOptions;
+
+const DEFAULT_REDIRECT_CALLBACK: callbackFunction = () =>
+  window.history.replaceState({}, document.title, window.location.pathname);
+
+export const Auth0Context = React.createContext<string | null>(null);
+
 export const Auth0Provider: React.FC<Auth0ClientProps> = ({
-  onRedirectCallback,
+  onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
 }) => {
   return <div>1</div>;
 };
