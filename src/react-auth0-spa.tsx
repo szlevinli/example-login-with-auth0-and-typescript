fd76b01c03178743da78f1b155c94dfa95773ee0
levin <3259963@qq.com>
2020-05-14 22:58:54 +0800

    update react-auth0-spa.tsx

--- src/react-auth0-spa.tsx
+++ src/react-auth0-spa.tsx
@@ -1,19 +1,44 @@
 import React, { useEffect, useState, useContext } from 'react';
-import createAuth0Client, { Auth0ClientOptions } from '@auth0/auth0-spa-js';
+import createAuth0Client, {
+  Auth0ClientOptions,
+  PopupLoginOptions,
+  RedirectLoginOptions,
+  IdToken,
+  GetIdTokenClaimsOptions,
+  GetTokenSilentlyOptions,
+  GetTokenWithPopupOptions,
+  LogoutOptions,
+  RedirectLoginResult,
+} from '@auth0/auth0-spa-js';
+import { createCtx } from './utils/createCtx';
 
-type callbackFunction = {
-  (): void;
+type Auth0ClientContextType = {
+  isAuthenticated: boolean;
+  user: any;
+  loading: boolean;
+  popupOpen: boolean;
+  loginWithPopup(options: PopupLoginOptions): Promise<void>;
+  handleRedirectCallback(): Promise<void>;
+  getIdTokenClaims(options?: GetIdTokenClaimsOptions): Promise<IdToken>;
+  loginWithRedirect(options: RedirectLoginOptions): Promise<void>;
+  getTokenSilently(
+    options?: GetTokenSilentlyOptions
+  ): Promise<string | undefined>;
+  getTokenWithPopup(options?: GetTokenWithPopupOptions): Promise<string>;
+  logout(options?: LogoutOptions): void;
 };
-export type Auth0ClientProps = {
-  onRedirectCallback: callbackFunction;
-} & Auth0ClientOptions;
 
-const DEFAULT_REDIRECT_CALLBACK: callbackFunction = () =>
+const DEFAULT_REDIRECT_CALLBACK = () =>
   window.history.replaceState({}, document.title, window.location.pathname);
+
+type Auth0ClientProps = {
+  onRedirectCallback?(result: RedirectLoginResult): void;
+} & Auth0ClientOptions;
 
 export const Auth0Context = React.createContext<string | null>(null);
 
 export const Auth0Provider: React.FC<Auth0ClientProps> = ({
+  children,
   onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
 }) => {
   return <div>1</div>;
